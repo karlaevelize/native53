2599cd277c32edf17359356917dc6119f90859bc
karlaevelize <k.evelize@gmail.com>
2021-10-21 21:14:44 +0200

    README + code

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import axios from "axios"
+import { ScrollView, Text, Image, Button, Alert, TouchableOpacity, View, StyleSheet} from 'react-native';
 
 export default function App() {
+
+  const [ data, setData ] = useState([])
+
+  const fetchData = async () => {
+    const response = await axios.get("https://hp-assessment-api.herokuapp.com/characters") 
+    console.log("response", response)
+    setData(response.data)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={{marginVertical: 60}}>
+      <Text style={{fontSize: 24, textAlign: "center", fontWeight: "bold"}}>
+        Hello from React Native!
+      </Text>
+      <TouchableOpacity onPress={() => Alert.alert("You lazy coder!")}>
+        <Image
+          source={{uri: "https://www.semicolonworld.com/uploads/memes/gmOAZ1nNYV.jpg"}}
+          style={{width: "100%", height: 400, marginVertical: 20}}
+        />
+      </TouchableOpacity>
+      <Button title="Test" color="green" onPress={() => Alert.alert("You clicked me!")}/>
+      {data.map(char => {
+        return <View key={char.id}>
+          <Text style={styles.nameTitle}>{char.name}</Text>
+          <Image source={{uri: char.imgUrl}} style={{width: "100%", height: 400, marginVertical: 20,}}/>
+          </View>     
+      })}
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  nameTitle: {
+    fontSize: 18,
+    textAlign: "center",
+    fontWeight: "bold"
+  }
+})
---
+++ README.md
@@ -0,0 +1,25 @@
+## Native App vs Cross Platform
+
+**iOS native:** Objective-C and Swift
+
+**Android native:** Java and Kotlin
+
+**Hybrid/Cross platform:** React Native and Kotlin
+
+### Native App Benefits
+
+- Better performance and commpatibility
+- App bundle is smaller/lighter
+
+### Hybrid/Cross platform Benefits
+
+- Easier to code if you know JS
+- No need to hire different developers (iOS and Android developers)
+- The company can use their frontend developers for the task
+- Cheaper
+
+## Main differences between React and React Native
+
+1. Phones don’t understand HTML (browsers do) so we don’t use HTML tags, we have components that substitute them 
+
+2. Routing. Why? URLs aren’t a thing so attaching a screen/component to one doesn’t make sense. We have a substitute called navigators.
